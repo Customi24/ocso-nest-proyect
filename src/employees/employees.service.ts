2c36afd5063197a36d9015e896bfc729c75adc87
Customi24 <131020560+Customi24@users.noreply.github.com>
2024-09-09 09:42:37 -0600

    Database config

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,19 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
-import { Employee } from './entities/employee.entity';
+import {v4 as uuid} from 'uuid';
 
 @Injectable()
 export class EmployeesService {
   private employees: CreateEmployeeDto[] = [
     {
-      id: 1,
+      id: uuid(),
       name: "Mau",
       lastName: "Morales",
       phoneNumber: "4613027231"
     },
     {
-      id: 2,
+      id: uuid(),
       name: "Jasiel",
       lastName: "Salmeron",
       phoneNumber: "4423027231"
@@ -22,7 +22,7 @@ export class EmployeesService {
 
 
   create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = this.employees.length + 1
+    createEmployeeDto.id = uuid()
     this.employees.push(createEmployeeDto);
     return createEmployeeDto;
   }
@@ -32,18 +32,20 @@ export class EmployeesService {
     return this.employees;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     const employee = this.employees.filter((employee)=>employee.id == id)[0];
+    if (!employee) throw new NotFoundException();
     return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
     let employeeToUpdate = this.findOne(id);
     employeeToUpdate = {
       ...employeeToUpdate,
       ...updateEmployeeDto,
 
     }
+    if (employeeToUpdate) throw new NotFoundException();
     this.employees = this.employees.map((employee)=>{
       if (employee.id==id){
         employee=employeeToUpdate
@@ -53,7 +55,8 @@ export class EmployeesService {
     return employeeToUpdate;
   }
 
-  remove(id: number) {
+  remove(id: string) {
+    this.findOne(id)
     this.employees = this.employees.filter((employee) => employee.id == id);
     return this.employees;
   }
