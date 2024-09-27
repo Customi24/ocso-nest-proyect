0f35236ef49a455ba91b392c93fb3b879192d9f3
Customi24 <131020560+Customi24@users.noreply.github.com>
2024-09-27 11:55:46 -0500

    Initial config for location route

--- src/locations/entities/location.entity.ts
+++ src/locations/entities/location.entity.ts
@@ -1,1 +1,15 @@
-export class Location {}
+import { combineLatest } from "rxjs";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+
+@Entity()
+export class Location {
+    @PrimaryGeneratedColumn('increment')
+    locationId: number;
+    @Column('text')
+    locationName: string;
+    @Column('text')
+    locationAdress: string;
+    @Column('array')
+    locationLatLng: number[];
+}
