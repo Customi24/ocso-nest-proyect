08c868bbf3665d04ded55b377bf23bc52d581297
Customi24 <131020560+Customi24@users.noreply.github.com>
2024-10-19 16:50:59 -0500

    fix: Return location on create

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -23,10 +23,13 @@ export class AuthController {
   @Post('login')
   login(@Body() loginUserDto :LoginUserDto, @Res({passthrough: true}) response: Response, @Cookies() cookies: any){
     const token = this.authService.loginUser(loginUserDto)
+    let expireDate = new Date()
+    expireDate.setDate(expireDate.getDay() * 7)
     response.cookie(TOKEN_NAME, token,{
       httpOnly: false,
       secure: true,
       sameSite: 'none',
+      expires: expireDate,
       maxAge: 1000 * 60 * 60 * 24 * 7,
     });
 
