ca34fb2b628f2b88f8f06a986afd0fe472d49694
Customi24 <131020560+Customi24@users.noreply.github.com>
2024-11-14 09:53:52 -0600

    139-RegisterManager

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -20,7 +20,11 @@ export class EmployeesService {
   }
 
   findAll() {
-    return this.employeeRepository.find();
+    return this.employeeRepository.find({
+      relations: {
+        location: true
+      }
+    });
   }
 
 
@@ -33,8 +37,13 @@ export class EmployeesService {
   }
 
   findOne(id: string) {
-    const employee = this.employeeRepository.findOneBy({
-      employeeId: id
+    const employee = this.employeeRepository.findOne({
+      where:{
+        employeeId: id
+      },
+      relations: {
+        location: true,
+      }
     })
     return employee;
   }
